c3d3445e278cc13b9d350a04d1dc0893b98c2418
24030021 <24030021@saultcollege.ca>
2026-02-10 16:48:58 -0500

    lab 3 in progress

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,12 +1,25 @@
-/*
-    NOTE:
 
-    Add to this type any variables and/or methods required
-    to represent one playing card.
 
-    You MAY change this to a record/enum as you see fit.
- */
-package core;
+public record Card (Rank rank, Suit suit){
+    static public enum Suit {
+        HEARTS, DIAMONDS, CLUBS, SPADES
+    }
+    static public enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
 
-public class Card {
-}
+    public int value() {
+        return switch (this) {
+            case TWO -> 2;
+            case THREE -> 3;
+            case FOUR -> 4;
+            case FIVE -> 5;
+            case SIX -> 6;
+            case SEVEN -> 7;
+            case EIGHT -> 8;
+            case NINE -> 9;
+            case TEN, kING, JACk,QUEEN -> 10;
+            case ACE -> 11;
+
+        }
+    }
+
+    }
--- src/main/java/core/CardStack.java
+++ src/main/java/core/CardStack.java
@@ -8,5 +8,19 @@
  */
 package core;
 
-public class m       CardStack {
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class  CardStack {
+    public static List<Card> gimmeADeck() {
+        ArrayList<Card> deck = new ArrayList<>();
+        for (Card.Suit suit : Card.Rank.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                deck.add( new Card(rank, suit));
+            }
+        }
+        Collections.shuffle(deck);
+        return deck;
 }
